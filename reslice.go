93af9e54e1e35f6985b83518c2652fce8006837b
Liberbell <liberbell@gmail.com>
2020-12-14 08:55:55 +0900

    reslice4

--- reslice.go
+++ reslice.go
@@ -6,5 +6,7 @@ func main() {
 	slice := []int{7, 8, 9, 10, 15}
 	fmt.Println("Slice == ", slice)
 
-	fmt.Println("Slice[1:4", slice[1:4])
+	fmt.Println("Slice[1:4]", slice[1:4])
+
+	fmt.Println("Slice[:3]", slice[:3])
 }
