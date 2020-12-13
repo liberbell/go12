d3743cca1a15f80bd4fda21ce2f359c7181840fd
Liberbell <liberbell@gmail.com>
2020-12-14 08:52:13 +0900

    slices2

--- slices.go
+++ slices.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	slice := []int{7, 8, 9, 10, 12}
 	fmt.Println("Slice  ==", slice)
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Printf("Slice[%d] == %d\n", i, slice[i])
+	}
 }
