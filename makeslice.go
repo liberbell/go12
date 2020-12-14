06836d57f3fb6428b43afe7af5d4c4c0be4f2d4f
Liberbell <liberbell@gmail.com>
2020-12-15 08:58:47 +0900

    makeslice5

--- makeslice.go
+++ makeslice.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func printS1(s string, x []int) {
-	fmt.Printf("%s len= %d cap= %d %v\n", s, len[x], cap[x], x)
+	fmt.Printf("%s len= %d cap= %d %v\n", s, len(x), cap(x), x)
 }
 
 func main() {
