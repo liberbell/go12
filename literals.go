82ee97bfc26873dbd493e834f61bad318ef3145c
Liberbell <liberbell@gmail.com>
2020-12-16 08:56:55 +0900

    literals4

--- literals.go
+++ literals.go
@@ -9,8 +9,10 @@ type Rect struct {
 var (
 	r1 = Rect{7, 8}     // type Rect
 	r2 = Rect{Width: 4} // Height is implictly 0
+	r3 = Rect{}         // Width and Height both 0
+	p  = &Rect{7, 8}    // type * Rect
 )
 
 func main() {
-	fmt.Println()
+	fmt.Println(r1, r2, r3)
 }
