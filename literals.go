f92d9cbfe58810e318f81eb3f6577af9d098f1ca
Liberbell <liberbell@gmail.com>
2020-12-16 08:55:38 +0900

    literals3

--- literals.go
+++ literals.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 type Rect struct {
 	Width, Height int
+}
+
+var (
+	r1 = Rect{7, 8}     // type Rect
+	r2 = Rect{Width: 4} // Height is implictly 0
+)
+
+func main() {
+	fmt.Println()
 }
