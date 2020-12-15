29c53c239e0100efd0fe9ee25ddcc39d7db28594
Liberbell <liberbell@gmail.com>
2020-12-16 08:49:58 +0900

    structs6

--- structs.go
+++ structs.go
@@ -9,7 +9,12 @@ type Rect struct {
 
 func main() {
 	// fmt.Println(Rect{7, 8})
-	r := Rect{1, 2}
-	r.Width = 18
-	fmt.Println(r.Width, r.Height)
+	// r := Rect{1, 2}
+	// r.Width = 18
+	// fmt.Println(r.Width, r.Height)
+
+	r := Rect{4, 6}
+	p := &r
+	p.Width = 8
+	fmt.Println(r)
 }
