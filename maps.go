b38047cd74602e7b23edba6cd1f506fb4569ee3c
Liberbell <liberbell@gmail.com>
2020-12-17 08:35:51 +0900

    maps4

--- maps.go
+++ maps.go
@@ -1,10 +1,16 @@
 package main
 
+import "fmt"
+
 type Rect struct {
 	Width, Height int
 }
 
-var m = map[string]Rect {
-	"Rect1" : Rect{1, 2,},
-	"Rect2" : Rect{4, 6}
+var m = map[string]Rect{
+	"Rect1": Rect{1, 2},
+	"Rect2": Rect{4, 6},
+}
+
+func main() {
+	fmt.Println(m)
 }
