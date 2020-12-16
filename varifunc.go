453ef373669548a97844eed3366c7145b869e44f
Liberbell <liberbell@gmail.com>
2020-12-17 08:46:55 +0900

    varifunc2

--- varifunc.go
+++ varifunc.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func add(args ...int) int {
+	total := 0
+	for _, n := range args {
+		total += n
+	}
+	return total
+}
+
+func main() {
+	fmt.Println(add(8, 13, 4, 6, 3))
+}
