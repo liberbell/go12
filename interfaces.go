8dc9e7aa675ee57fa052cedaad60ac106f340771
Liberbell <liberbell@gmail.com>
2020-12-19 17:12:14 +0900

    interfaces3

--- interfaces.go
+++ interfaces.go
@@ -3,3 +3,10 @@ package main
 type Calcer interface{ Calc() float64 }
 type Square struct{ X, Y float64 }
 type myFloat float64
+
+func (f myFloat) Calc() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
