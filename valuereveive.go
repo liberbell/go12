a8b90bc28c68e2d3830ee79d0de7264eb084497e
Liberbell <liberbell@gmail.com>
2020-12-19 17:06:24 +0900

    valuerevieve4

--- valuereveive.go
+++ valuereveive.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 type Root float64
 
 func (r Root) Abs() float64 {
@@ -7,4 +12,9 @@ func (r Root) Abs() float64 {
 		return float64(-r)
 	}
 	return float64(r)
+}
+
+func main() {
+	r := Root(-math.Sqrt2)
+	fmt.Println(r.Abs())
 }
