f4c41cc7af664073f63b0f1bc4b41e35ce8778bf
Liberbell <liberbell@gmail.com>
2020-12-20 11:34:57 +0900

    concurrency3

--- concurrency.go
+++ concurrency.go
@@ -1,8 +1,15 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	fmt.Println("\nMessage from func main, I'm finished.")
+}
+
+func msg() {
 	for i := 0; i <= 10; i++ {
 		time.Sleep(time.Microsecond * 1000)
 	}
