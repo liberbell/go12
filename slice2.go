d97d64e162fe4171a74bd1eaeda3f644909eb300
Liberbell <liberbell@gmail.com>
2020-12-21 08:52:30 +0900

    slice2-4

--- slice2.go
+++ slice2.go
@@ -7,4 +7,8 @@ func main() {
 	fmt.Println("\nHere is our slice: ")
 	fmt.Println("slice == ", slice)
 	fmt.Println("slice[1:4]", slice[1:4])
+	fmt.Println("slice[:3]", slice[:3])
+	fmt.Println("slice[2:]", slice[2:])
+	fmt.Println("len slice ==", len(slice))
+	fmt.Println("cap slice ==", cap(slice))
 }
